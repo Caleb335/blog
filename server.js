12ebf6aebc352c49d0a40a0a51d5317a8d456125
Caleb335 <belac335@gmail.com>
2020-09-25 23:51:09 +0100

    add index view template

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express')
 
 // assigning the `express` var to the
 // app instance.
-
 const app = express()
 
-app.get(res, req => {
-    res.send('Hello node blog')
+// setting the view (ejs) so 
+// actual html markup gets send to
+// the browser
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
 })
+
+app.listen(8000) 
